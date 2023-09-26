a5eae0c653d3a4f58e6d67bf38b85e5e2c97401d
eric-aurora <eric.pool@cuw.edu>
2023-09-25 22:48:48 -0500

    Fixing json parse

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
 
 (function ($) {
 
-    var obj;
     fetch('data.json')
         .then((response) => response.json())
         .then((json) => {
-            obj = json
-        });
-
-    let parsed = JSON.parse(obj);
+            let parsed = JSON.parse(json);
 
 
-    rows = ``;
-    for (const item of parsed) {
-        const value = item[1];
-        const root = value.roots.join(', ');
-        const senses = value.senses.join('; ');
-        const count = value.count;
-        rows += `
+            rows = ``;
+            for (const item of parsed) {
+                const value = item[1];
+                const root = value.roots.join(', ');
+                const senses = value.senses.join('; ');
+                const count = value.count;
+                rows += `
             <tr>
                 <td>${count}</td>
                 <td>${root}</td>
                 <td>${senses}</td>
             </tr>
             `
-    }
+            }
 
-    let body = document.getElementById('table-wrapper');
-    console.log(body)
+            let body = document.getElementById('table-wrapper');
+            console.log(body)
 
-    table = `<table class="table">
-        <thead>
-        <tr>
-            <th scope="col">Count</th>
-            <th scope="col">Word</th>
-            <th scope="col">First</th>
-        </tr>
-        </thead>
-        <tbody>
-            ${rows}
-        </tbody>
-    </table>
-    `
+            table = `<table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Count</th>
+                        <th scope="col">Word</th>
+                        <th scope="col">First</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        ${rows}
+                    </tbody>
+                </table>
+                `
 
 
-    body.innerHTML = table
+            body.innerHTML = table
+        });
+
+
 })(jQuery)
