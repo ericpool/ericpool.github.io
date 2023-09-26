f20bdaa5c3233c5e8176cf18243b015313202aed
eric-aurora <eric.pool@cuw.edu>
2023-09-26 12:34:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,114 +1,11 @@
 
 (function ($) {
 
-    // fetch('data.json')
-    //     .then((response) => response.json())
-    //     .then((json) => {
-    //         let parsed = json.result;
-    //         console.log(parsed);
-
-
-    let parsed = JSON.parse(`
-    {
-        "result": [
-            [
-                29538,
-                {
-                    "actual": [
-                        "paulo",
-                        "paulus",
-                        "pauli",
-                        "paulum"
-                    ],
-                    "count": 420,
-                    "roots": [
-                        "paul",
-                        "paul"
-                    ],
-                    "senses": [
-                        "Paul"
-                    ],
-                    "word_type": "Noun"
-                }
-            ],
-            [
-                412,
-                {
-                    "actual": [
-                        "accipimus",
-                        "accepta",
-                        "accepissemus",
-                        "accepimus",
-                        "accipiens",
-                        "accipi",
-                        "accipitur",
-                        "accipiat",
-                        "accipere",
-                        "accipiant",
-                        "accipite",
-                        "acceperunt",
-                        "acciperemus",
-                        "accipit",
-                        "accipiamus",
-                        "accipiebant",
-                        "accipientis",
-                        "accepto",
-                        "accepti",
-                        "acceptos",
-                        "accipiatur",
-                        "acceptum",
-                        "accepisse",
-                        "acceperant",
-                        "accipiendam",
-                        "accipiet",
-                        "accipiunt",
-                        "accipientes",
-                        "accipienda",
-                        "accipiantur",
-                        "acceptis",
-                        "acceptae",
-                        "accepissent",
-                        "accepit",
-                        "acceptas",
-                        "accipias",
-                        "accipient",
-                        "acceptus",
-                        "acceptorum",
-                        "acceperit",
-                        "acceperint",
-                        "accepturus",
-                        "accipiendum",
-                        "acceperimus",
-                        "accepissem",
-                        "accepisti",
-                        "acceperis",
-                        "acceptam",
-                        "accipiuntur",
-                        "acceptaque",
-                        "acceperat",
-                        "acciperent",
-                        "accipientibus",
-                        "accepi"
-                    ],
-                    "count": 404,
-                    "roots": [
-                        "accipi",
-                        "accip",
-                        "accep",
-                        "accept"
-                    ],
-                    "senses": [
-                        "take, grasp, receive, accept, undertake",
-                        "admit, let in, hear, learn",
-                        "obey"
-                    ],
-                    "word_type": ["Verb", "Adverb"]
-                }
-            ]
-        ]
-    }
-    `).result
-
+    fetch('data.json')
+        .then((response) => response.json())
+        .then((json) => {
+            let parsed = json.result;
+            console.log(parsed);
 
             rows = ``;
             for (const item of parsed) {
@@ -156,7 +53,7 @@
 
 
             body.innerHTML = table
-        // });
+        });
 
         $(document).on('click', '.show-modal-custom', function(){
             const element = document.getElementById('modal-wrapper')
